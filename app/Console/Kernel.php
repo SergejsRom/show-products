4644652f8ede5ad85e4af1f3956d627c0420228d
SergejsRom <sergejsromanovskis@gmail.com>
2023-04-15 13:49:18 +0300

    runing schedule on local with shedule:work

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,15 +4,17 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\Console\Commands\StockUpdate;
 
 class Kernel extends ConsoleKernel
 {
+    protected $commands = [StockUpdate::class];
     /**
      * Define the application's command schedule.
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command(StockUpdate::class, ['schedule'])->everyMinute();
     }
 
     /**
@@ -20,7 +22,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }

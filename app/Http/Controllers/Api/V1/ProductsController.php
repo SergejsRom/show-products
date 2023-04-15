263b5323eb418cc95735d7981dd6e19674d9233e
SergejsRom <sergejsromanovskis@gmail.com>
2023-04-15 16:28:54 +0300

    Api cache, basic for 60 sec

--- app/Http/Controllers/Api/V1/ProductsController.php
+++ app/Http/Controllers/Api/V1/ProductsController.php
@@ -5,11 +5,14 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use App\Models\Product;
 use App\Http\Resources\ProductResource;
+use Illuminate\Support\Facades\Cache;
 
 class ProductsController extends Controller
 {
     public function index()
     {
-        return ProductResource::collection(Product::all());
+        return ProductResource::collection(Cache::remember('sku', 60, function () {
+            return Product::all();
+        }));
     }
 }
--- app/Http/Controllers/Api/V1/StockController.php
+++ app/Http/Controllers/Api/V1/StockController.php
@@ -5,11 +5,14 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use App\Models\Stock;
 use App\Http\Resources\StockResource;
+use Illuminate\Support\Facades\Cache;
 
 class StockController extends Controller
 {
     public function index()
     {
-        return StockResource::collection(Stock::all());
+        return StockResource::collection(Cache::remember('sku', 60, function () {
+            return Stock::all();
+        }));
     }
 }

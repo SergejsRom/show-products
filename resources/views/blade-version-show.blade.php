399287cc96125fb2ab5b3bc27cfa4b1266416209
SergejsRom <sergejsromanovskis@gmail.com>
2023-04-17 00:07:17 +0300

    .

--- resources/views/blade-version-show.blade.php
+++ resources/views/blade-version-show.blade.php
@@ -20,7 +20,7 @@
 
     <div
         class="self-center items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 p-4 mt-8">
-        <img class="justify-center object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+        <img class="object-cover w-full rounded-lg h-96 md:h-auto md:w-48 "
             src="../storage/{{ $product->photo }}" alt="{{ $product->photo }}">
         <div class="flex flex-col justify-between p-4 leading-normal">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ $product->sku }}</h5>

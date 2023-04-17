af0e42db0672e876a81dd41d7d3d6695a70320b8
SergejsRom <sergejsromanovskis@gmail.com>
2023-04-17 22:55:59 +0300

    API scribe documentation

--- app/Http/Controllers/Api/V1/Auth/LoginController.php
+++ app/Http/Controllers/Api/V1/Auth/LoginController.php
@@ -8,6 +8,10 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
+
+/**
+ * @group Auth
+ */
  
 class LoginController extends Controller
 {

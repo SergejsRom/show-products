4d599987818252041827a3c43e200a57d5f3d115
SergejsRom <sergejsromanovskis@gmail.com>
2023-04-18 16:17:24 +0300

    restrict create new from jetstream,only 1 from api

--- app/Http/Controllers/Api/V1/Auth/RegisterController.php
+++ app/Http/Controllers/Api/V1/Auth/RegisterController.php
@@ -18,24 +18,30 @@ class RegisterController extends Controller
 {
     public function __invoke(Request $request)
     {
-        $request->validate([
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'password' => ['required', 'confirmed', Password::defaults()],
-        ]);
- 
-        $user = User::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
-        ]);
- 
-        event(new Registered($user));
- 
-        $device = substr($request->userAgent() ?? '', 0, 255);
- 
-        return response()->json([
-            'access_token' => $user->createToken($device)->plainTextToken,
-        ], Response::HTTP_CREATED);
+        if ( User::count() >= 1) {
+            return response()->json([
+                'Operation restricted'
+            ], 403);
+        } else {
+            $request->validate([
+                'name' => ['required', 'string', 'max:255'],
+                'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+                'password' => ['required', 'confirmed', Password::defaults()],
+            ]);
+     
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+     
+            event(new Registered($user));
+     
+            $device = substr($request->userAgent() ?? '', 0, 255);
+     
+            return response()->json([
+                'access_token' => $user->createToken($device)->plainTextToken,
+            ], Response::HTTP_CREATED);
+        }
     }
 }

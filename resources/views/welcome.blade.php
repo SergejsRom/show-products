03bbc69e91bbc8ec637902d8772488ac7f940906
SergejsRom <sergejsromanovskis@gmail.com>
2023-04-21 10:47:36 +0300

    telescope

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,12 +24,12 @@
                 <div class="-mt-1 font-sans text-lg font-semibold">Login</div>
             </div>
         </a>
-        <a href="{{ route('register') }}"
+        {{-- <a href="{{ route('register') }}"
             class="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800">
             <div class="text-left">
                 <div class="-mt-1 font-sans text-lg font-semibold">Register</div>
             </div>
-        </a>
+        </a> --}}
     </div>
 
     <div
